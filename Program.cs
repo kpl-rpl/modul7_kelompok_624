f571539cd05bdbf5d3398719c6e893daab81caab
unknown <akunset20@gmail.com>
2026-04-20 23:28:51 +0700

    Menambahkan class TeamMembers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Jangan lupa sesuaikan nama class dengan NIM-mu
+        TeamMembers103082400035 team = new TeamMembers103082400035();
+        team.ReadJSON();
+    }
+}
